df2f4fd88d2ee1cb834e9eec7e8ad94ab3c085ff
ReverendEd <tysehr37@gmail.com>
2020-01-10 13:56:06 -0800

    added image variable size support

--- app/src/GreenFeedCard.js
+++ app/src/GreenFeedCard.js
@@ -3,19 +3,41 @@ import React from 'react'
 class GreenFeedCard extends React.Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			imageStyle: 'highres'
+		}
 	}
+
+	
 
 	render() {
 
 		console.log(this.props.article);
+
+		let imageStyle = 'highres'
+
+		let reactThis = this;
+
+		let img = new Image();
+		img.src = this.props.article.urlToImage;
+		img.onload = function () {
+			if (this.width < 200) {
+				reactThis.setState({
+					imageStyle: 'lowres'
+				})
+				
+			}
+		}
 		
+
 
 		return (
 			<div className="card">
-				<img src={this.props.article.urlToImage}  alt="Avatar" />
+				<img className={this.state.imageStyle} src={this.props.article.urlToImage} alt="Avatar" />
 				<div className="container">
 					<h4><b>{this.props.article.title}</b></h4>
-					<p className="description">{this.props.article.description.substring(0, 100)}</p>
+					<p className="description">{this.props.article.description.substring(0, 100) + '...'}</p>
 					<p className="sourcename">{this.props.article.source.name}</p>
 				</div>
 			</div>
