72056b1cad62a0e6473c75c4497a8d6d48ed3e00
ReverendEd <tysehr37@gmail.com>
2020-01-10 14:47:33 -0800

    made links clickable

--- app/src/GreenFeedCard.js
+++ app/src/GreenFeedCard.js
@@ -13,7 +13,7 @@ class GreenFeedCard extends React.Component {
 
 	render() {
 
-		console.log(this.props.article);
+		// console.log(this.props.article);
 
 		let reactThis = this;
 		let img = new Image();
@@ -31,7 +31,8 @@ class GreenFeedCard extends React.Component {
 				<img className={this.state.imageStyle} src={this.props.article.urlToImage} alt="Avatar" />
 				<div className="container">
 					<h4><b>{this.props.article.title}</b></h4>
-					<p className="description">{this.props.article.description.substring(0, 100) + '...'}</p>
+					<hr></hr>
+					<p className="description">{this.props.article.description.substring(0, 150) + '...'}</p>
 					<p className="sourcename">{this.props.article.source.name}</p>
 				</div>
 			</div>
